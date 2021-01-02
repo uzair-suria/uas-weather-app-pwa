ed69db26fa8713bf88e7dd30db361bcf3d132054
Uzair Suria <uzair.a.suria@outlook.com>
2021-01-02 14:31:59 +0500

    initialized the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<div style={{ textAlign: 'center' }}>
+				<h1>Weather Application Deployed as PWA</h1>
+				<p>
+					The application is under developement at the moment. Please visit
+					later...
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default App;
