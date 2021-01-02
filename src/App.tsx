f12ca6cff4ac81075859921abd3185c595696dba
Uzair Suria <uzair.a.suria@outlook.com>
2021-01-02 19:10:47 +0500

    implemented initial api logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
+
+import './App.css';
 
 const App: React.FC = () => {
+	const [query, setQuery] = useState<string>('');
+
+	const search = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			const data = await fetchWeather(query);
+			console.log(data);
+		}
+	};
+
 	return (
-		<div>
-			<div style={{ textAlign: 'center' }}>
-				<h1>Weather Application Deployed as PWA</h1>
-				<p>
-					The application is under developement at the moment. Please visit
-					later...
-				</p>
-			</div>
+		<div className="main-container">
+			<input
+				type="text"
+				className="search"
+				placeholder="Enter a city name..."
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				onKeyPress={search}
+			/>
 		</div>
 	);
 };
