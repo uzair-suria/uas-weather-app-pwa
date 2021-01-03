ce2717fcc0938ff53267d93d574ccec844d6a0f2
Uzair Suria <uzair.a.suria@outlook.com>
2021-01-03 18:54:32 +0500

    implemented PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { fetchWeather } from './api/fetchWeather';
+import { fetchWeather, WeatherObject } from './api/fetchWeather';
 
 import './App.css';
 
 const App: React.FC = () => {
 	const [query, setQuery] = useState<string>('');
+	const [weather, setWeather] = useState<WeatherObject>();
 
 	const search = async (e: React.KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
-			const data = await fetchWeather(query);
-			console.log(data);
+			const data: WeatherObject = await fetchWeather(query);
+			setWeather(data);
+			setQuery('');
+			console.log(weather);
 		}
 	};
 
@@ -23,6 +26,26 @@ const App: React.FC = () => {
 				onChange={(e) => setQuery(e.target.value)}
 				onKeyPress={search}
 			/>
+			{weather?.main && (
+				<div className="city">
+					<h2 className="city-name">
+						<span>{weather.name}</span>
+						<sup>{weather.sys.country}</sup>
+					</h2>
+					<div className="city-temp">
+						{Math.round(weather.main.temp)}
+						<sup>&deg;C</sup>
+					</div>
+					<div className="info">
+						<img
+							src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+							alt={weather.weather[0].description}
+							className="city-icon"
+						/>
+						<p>{weather.weather[0].description}</p>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/api/fetchWeather.ts
+++ src/api/fetchWeather.ts
@@ -3,13 +3,55 @@ import axios from 'axios';
 const URL = `https://api.openweathermap.org/data/2.5/weather`;
 const API_KEY = `f33a484cf794d08d0148764789aaba32`;
 
-export const fetchWeather = async (query: string): Promise<object> => {
-	const { data } = await axios.get(URL, {
+export type WeatherObject = {
+	base: string;
+	clouds: { all: number };
+	cod: number;
+	coord: { lat: number; lon: number };
+	dt: number;
+	id: number;
+	main: {
+		feels_like: number;
+		humidity: number;
+		pressure: number;
+		temp: number;
+		temp_max: number;
+		temp_min: number;
+	};
+	name: string;
+	sys: {
+		country: string;
+		id: number;
+		sunrise: number;
+		sunset: number;
+		type: number;
+	};
+	timezone: number;
+	visibility: number;
+	weather: { id: number; main: string; description: string; icon: string }[];
+	wind: { deg: number; speed: number };
+};
+
+// export type WeatherResponse = WeatherObject | undefined;
+export type WeatherResponse =
+	| undefined
+	| {
+			config: object;
+			data: WeatherObject;
+			headers: object;
+			request: object;
+			status: number;
+			statusText: string;
+	  };
+
+export const fetchWeather = async (query: string) => {
+	const res = await axios.get(URL, {
 		params: {
 			q: query,
 			units: 'metric',
 			APPID: API_KEY,
 		},
 	});
+	const data: WeatherObject = res.data;
 	return data;
 };
